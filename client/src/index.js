5dc7e81e1fbaee4c6335fd17ed00cb88accf6a25
andisaba <71749082+andisaba@users.noreply.github.com>
2021-12-10 19:06:15 +1100

    updated serviceworker code due to error

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 import App from './App';
 
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorker from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -27,12 +27,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// serviceWorker.register();
+serviceWorker.register();
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.unregister();
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
