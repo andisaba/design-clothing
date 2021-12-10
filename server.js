cc7270ad58158ab5320c8c67c5a51e3633e520ff
andisaba <71749082+andisaba@users.noreply.github.com>
2021-12-10 17:46:02 +1100

    updating code to be pwa compliant, added serviceworker, https, updated manifest

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
+const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -9,11 +11,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+// app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression);  /* located here for production*/
+  app.use(enforce.HTTPS({ trustProtoHeader: true })); /* located here for production*/
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -25,6 +31,10 @@ app.listen(port, error => {
   if (error) throw error;
   console.log('Server running on port' + port);
 });
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+})
 
 app.post('/payment', (req, res) => {
   const body = {
